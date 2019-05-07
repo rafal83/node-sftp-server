0e53c76208ab937a1a9d21ac1de832e4595a5493
Raph <rafal83@users.noreply.github.com>
2019-05-07 13:51:03 +0200

    Update node-sftp-server.js
    add wx flag

--- node-sftp-server.js
+++ node-sftp-server.js
@@ -372,6 +372,7 @@ var SFTPSession = (function(superClass) {
           return this.sftpStream.handle(reqid, handle);
         }.bind(this));
       case "w":
+      case "wx":
         rs = new Readable();
         started = false;
         rs._read = (function(_this) {
